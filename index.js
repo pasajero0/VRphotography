5944b703659cc5dd5c7a74c1ce7289a1e95ca255
Leonid <leomalinovsky@gmail.com>
2017-12-20 19:14:02 +0200

    full+mob ver fix 0.3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import tumblr from 'tumblr.js';
 import $ from 'jquery';
+import tumblr from 'tumblr.js';
+
 
 const client = tumblr.createClient({
     consumer_key: 'kVT2VuJt225CBW8wVs7uKg0Qv044wYdpJl5pVXnohQbjgpUeDu',
@@ -13,37 +14,64 @@ client.blogPosts('valen-romanovskaya.tumblr.com', {type: 'photo', tag: TagName, 
         posts.photos.forEach(function(photo) {
             const mediumPhoto = photo.alt_sizes[4];
             const largePhoto = photo.original_size;
-            const div = document.createElement( 'div' );
+            const div = document.createElement('div');
             if (mediumPhoto.height > mediumPhoto.width) {
-                $( div ).addClass( 'item2' );
+                $(div).addClass('item2');
             } else {
-                $( div ).addClass( 'item' );
+                $(div).addClass('item');
             }
-            $( div ).append('<img src="' + mediumPhoto.url + '" rel="' + largePhoto.url + '"> ');
+            $( div ).append('<img src="'+mediumPhoto.url+'" rel="'+largePhoto.url+'"> ');
             $('#photoalbum').append(div);
         });
     });
 });
 
 let elem = null;
+let elemClass = null;
+
+let ww = $(window).width();
+let wh = $(window).height();
 
 const getRel = () => {
     const thisLargePhoto = $(elem).children().attr('rel');
     $('div.GalleryPhotoSlide').empty();
-    $('.GalleryPhotoSlider').append('<div class="GalleryPhotoSlide"><img src="' + thisLargePhoto + '"></div>');
+    $('.GalleryPhotoSlider').append('<div class="GalleryPhotoSlide"><img src="'+thisLargePhoto+'"></div>');
+    if (ww > wh){
+        if(elemClass == 'item'){
+            $('.GalleryPhotoSlide>img').css('max-width','80vw');
+            $('.GalleryPhotoSlide>img').css('max-height','90vh');
+            console.log('if item');
+        }else if(elemClass == 'item2'){
+            $('.GalleryPhotoSlide>img').css('max-height','90vh');
+            $('.GalleryPhotoSlide>img').css('max-width','80vw');
+            console.log('if item2');
+        }
+        console.log("ww");
+    } else if (ww < wh){
+        if(elemClass == 'item'){
+            $('.GalleryPhotoSlide>img').css('max-width','80vw');
+            $('.GalleryPhotoSlide>img').css('max-height','42vh');
+            console.log('if item');
+        }else if(elemClass == 'item2'){
+            $('.GalleryPhotoSlide>img').css('max-height','90vh');
+            $('.GalleryPhotoSlide>img').css('max-width','75vw');
+            console.log('if item2');
+        }
+        console.log('wh');
+    }; 
 };
 const chevronDisplay = () => {
     const next = $(elem).next().children().attr('rel');
     const prev = $(elem).prev().children().attr('rel');
     if (prev == undefined) {
-    $('.backward>i').css('display', 'none');
+        $('.backward>i').css('display', 'none');
     } else {
-    $('.backward>i').css('display', 'block');   
+        $('.backward>i').css('display', 'block');   
     };
     if (next == undefined) {
-    $('.forward>i').css('display', 'none');
+        $('.forward>i').css('display', 'none');
     } else {
-    $('.forward>i').css('display', 'block');   
+        $('.forward>i').css('display', 'block');   
     };
 };
 
@@ -51,19 +79,50 @@ $('#photoalbum').on('click', 'div', (event) => {
     $('#overlay').css('display', 'block');
     $('body').css('overflow', 'hidden');
     elem = event.currentTarget;
+    elemClass = event.currentTarget.className;
     chevronDisplay();
     getRel();
 
     $('.backward').on('click', 'i', () => {
         elem = $(elem).prev();
+        elemClass = elem[0].className;
         chevronDisplay();
         getRel();
     });
     $('.forward').on('click', 'i', () => {
         elem = $(elem).next();
+        elemClass = elem[0].className;
         chevronDisplay();
         getRel();
     });
+
+    document.onkeyup = function (event) {
+        const next = $(elem).next().children().attr('rel');
+        const prev = $(elem).prev().children().attr('rel');
+        if(event.keyCode == 37){
+            if (prev == undefined) {
+                chevronDisplay();
+            } else {  
+                elem = $(elem).prev();
+                elemClass = elem[0].className;
+                chevronDisplay();
+                getRel();
+            };
+        } else if(event.keyCode == 39){
+            if (next == undefined) {
+                chevronDisplay();
+            } else { 
+                elem = $(elem).next();
+                elemClass = elem[0].className;
+                chevronDisplay();
+                getRel();
+            };
+        } else if(event.keyCode == 27){
+            $('#overlay').css('display', 'none');
+            $('body').css('overflow', 'auto');
+            $('div.GalleryPhotoSlide').empty();
+        };
+    };
 });
 
 $('.close-btn').on('click', () => {
